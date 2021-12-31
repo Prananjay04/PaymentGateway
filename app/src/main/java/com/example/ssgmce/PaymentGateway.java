ec4e02b1cf309a527d555bf5660e0d0dea99998b
Prananjay <prananjaykandekar12@gmail.com>
2021-12-31 15:10:17 +0530

    Testing

---
+++ app/src/main/java/com/example/ssgmce/PaymentGateway.java
@@ -0,0 +1,14 @@
+package com.example.ssgmce;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PaymentGateway extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.PaymentGateway);
+    }
+}
